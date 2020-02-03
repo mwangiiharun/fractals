503d0955e3f8992a9eec4ef2babccecf0b5630a3
mwangii <mwangiiharun@outlook.com>
2020-02-03 22:31:56 +0300

    Code cleanup

--- src/main/java/com/mwangii/fractals/mandelbrot/MandelbrotApplication.java
+++ src/main/java/com/mwangii/fractals/mandelbrot/MandelbrotApplication.java
@@ -24,10 +24,9 @@ public class MandelbrotApplication {
 
         for (int row = 0; row < height; row++) {
             for (int col = 0; col < width; col++) {
-                double c_re = (col - width/2)*4.0/width;
-                double c_im = (row - height/2)*4.0/width;
+                double c_re = ((col * 4.0) / width) - ((width >> 1) * 4.0 / width);
+                double c_im = ((row * 4.0) / width) - (((height >> 1) * 4.0) / width);
                 double x = 0, y = 0;
-                double r2;
                 int iteration = 0;
                 while (x*x+y*y < 4 && iteration < max) {
                     double x_new = x*x-y*y+c_re;
