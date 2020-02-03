a2bc51fc1169b27bd41d7d83298e39721d561a6f
mwangii <mwangiiharun@outlook.com>
2020-02-03 22:27:13 +0300

    First attempt at mandelbrot's set

---
+++ src/test/java/com/mwangii/fractals/mandelbrot/MandelbrotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mwangii.fractals.mandelbrot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MandelbrotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
